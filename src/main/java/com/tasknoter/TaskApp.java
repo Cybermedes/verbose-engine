b7ad460f7f69eac8daed3cc809d0291b035579e3
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-05-22 21:34:52 -0300

    feat: add methods to remove & complete tasks

--- src/main/java/com/tasknoter/TaskApp.java
+++ src/main/java/com/tasknoter/TaskApp.java
@@ -2,6 +2,7 @@ package com.tasknoter;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -18,9 +19,22 @@ class TaskApp {
         TaskAppUtil.saveUpdatedTasks(taskList);
     }
 
-    void updateTask(int taskNumber) {}
+    void completeTask(int index) {
+        if (index <= 0 || index > taskList.size()) {
+            System.out.println("invalid index value/ index out of range");
+            System.exit(0);
+        }
+        taskList.get(index).setCompleted(true);
+        taskList.get(index).setFinishedAt(LocalDateTime.now());
+        TaskAppUtil.saveUpdatedTasks(taskList);
+    }
 
-    void removeTask(int taskNumber) {}
-
-
+    void removeTask(int index) {
+        if (index <= 0 || index > taskList.size()) {
+            System.out.println("invalid index value/ index out of range");
+            System.exit(0);
+        }
+        taskList.remove(index);
+        TaskAppUtil.saveUpdatedTasks(taskList);
+    }
 }
